738e1f864d4cefed08dd2cdf674d36c1b2befa14
Marley <manfrededun@gmail.com>
2021-05-15 12:09:38 +0000

    updated changes4

--- src/Components/UserItem.js
+++ src/Components/UserItem.js
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { Modal } from 'react-bootstrap';
+import Editform from './Editform';
 
 function UserItem (props) {
+  const [isModalVisible, setShowModal] = useState(false);
+
+  function showModal() {
+    setShowModal(true);
+  }
+
+  function hideModal() {
+    setShowModal(false);
+  }
   return (
     <div className="row">
       <div className="col-md-3"><h4>Username</h4>{props.user.username}</div>
       <div className="col-md-3"><h4>Email</h4>{props.user.email}</div>
       <div className="col-md-2"><h4>Country</h4>{props.user.country}</div>
       <div className="col-md-4"><h4>Action</h4>
-        <button id="b1">Update</button>
+        <button id="b1" onClick={showModal}>Update</button>
+        <Modal show={isModalVisible} onHide={hideModal}>
+          <Modal.Header closeButton>
+            <Modal.Title>Edit User</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Editform
+              user={props.user}
+              updateUser={props.updateUser} 
+              hideModal={hideModal}
+            />
+          </Modal.Body>
+        </Modal>
+
         <button id="b2"
         onClick={() => { props.delete(props.user.id)}}>
           Delete</button>
