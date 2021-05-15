7dc48d611c4c3cd5845f25e7f472cb4f42adb7a0
Marley <manfrededun@gmail.com>
2021-05-15 12:09:22 +0000

    updated changes3

--- src/App.js
+++ src/App.js
@@ -4,10 +4,17 @@ import UserForm from './Components/UserForm';
 import UserList from './Components/UserList';
 import UserItem from './Components/UserItem';
 import { useState} from 'react';
+import Editform from './Components/Editform';
 
 function App() {
 
   const [users, setUsers] = useState([]);
+  const [userToUpdate, setUserToUpdate] = useState({
+    username: '',
+    email: '',
+    password: '',
+    country: '',
+  });
 
   function addUser(user) {
     setUsers([...users,user]);
@@ -17,7 +24,15 @@ function App() {
     let newUsers = users.filter((user) => user.id !== id);
     setUsers(newUsers);
   }
-
+  function updateUser(id, updatedUserInfo) {
+    let newUsers = users.map((user) => {
+      if (user.id === id) {
+        return updatedUserInfo;
+      }
+      return user;
+    });
+    setUsers(newUsers);
+  }
   return (
   <div className="container">
     <div className="row">
@@ -26,7 +41,7 @@ function App() {
       </div>
 
       <div className="col-md-6">
-        <UserList allUsers={users} delete={deleteUser} />
+        <UserList allUsers={users} delete={deleteUser} updateUser={updateUser}/>
       </div>
     </div>
   </div> 
